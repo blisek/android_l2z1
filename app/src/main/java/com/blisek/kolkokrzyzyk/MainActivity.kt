f112c334c70f1e631aad93c582b1aafaad29eb2c
blisek <bartlomiej.lisek@hotmail.com>
2017-03-20 00:48:06 +0100

    Gracz komputerowy

--- app/src/main/java/com/blisek/kolkokrzyzyk/MainActivity.kt
+++ app/src/main/java/com/blisek/kolkokrzyzyk/MainActivity.kt
@@ -11,12 +11,14 @@ import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private val ColumnCount = 3
-    private val RowsCount = 3
+    private val ColumnCount = 4
+    private val RowsCount = 4
     private val GridElementsTotal = ColumnCount * RowsCount
 
     private val board: Array<Turn>
-    private var currentTurn: Turn;
+    private val boardButtons: Array<Button?>
+    private var currentTurn: Turn
+    private var pcTurn: Turn
     private var gameInProgress: Boolean = true
     private var randomMoveGenerator: Random
     private lateinit var textView: TextView
@@ -24,8 +26,10 @@ class MainActivity : AppCompatActivity() {
 
     init {
         board = Array(GridElementsTotal, { Turn.UNKNOWN })
+        boardButtons = Array(GridElementsTotal, { null })
         currentTurn = Turn.CROSS
         randomMoveGenerator = Random()
+        pcTurn = Turn.CIRCLE
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,8 +50,8 @@ class MainActivity : AppCompatActivity() {
         for(i in 1..GridElementsTotal) {
             val btn = Button(this)
             btn.text = " "
-            btn.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT)
+            btn.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
             btn.tag = i-1
             btn.setOnClickListener { onBoardButtonClick(it) }
 
@@ -56,65 +60,186 @@ class MainActivity : AppCompatActivity() {
             val gridLayoutParams = GridLayout.LayoutParams(
                     GridLayout.spec(row), GridLayout.spec(column))
             gridLayout.addView(btn, gridLayoutParams)
+            boardButtons[i-1] = btn
         }
+
+        gridLayout.postInvalidate()
     }
 
     private fun onBoardButtonClick(btn: View) {
         if(gameInProgress) {
             val viewBtn = btn as? Button
             if (viewBtn != null) {
-                viewBtn.text = currentTurn.toString()
-                board[viewBtn.tag as Int] = currentTurn
+                nextMove(viewBtn)
             }
 
-            if(!checkWinConditions()) {
-                currentTurn = currentTurn.flip()
-                updateStatus()
-            }
+        }
+    }
+
+    private fun nextMove(viewBtn: Button) {
+        val tag = viewBtn.tag as Int
+        if(board[tag] != Turn.UNKNOWN)
+            return
+
+        board[tag] = currentTurn
+
+
+        viewBtn.text = currentTurn.toString()
+        if(checkWinConditions())
+            return
+        currentTurn = currentTurn.flip()
+
+        if(currentTurn == pcTurn)
+            nextPCMove()
+        else {
+            updateStatus()
         }
     }
 
     private fun checkWinConditions() : Boolean {
-        for (i in 0..GridElementsTotal-1 step 3) {
-            if(board[i] != Turn.UNKNOWN && board[i] == board[i+1] && board[i] == board[i+2]) {
-                endGame()
-                return true
-            }
-        }
-
-        for (i in 0..ColumnCount-1) {
-            if(board[i] != Turn.UNKNOWN && board[i] == board[i+ColumnCount] &&
-                    board[i] == board[i + ColumnCount*2]) {
-                endGame()
-                return true
-            }
-        }
-
-        if(board[0] != Turn.UNKNOWN && board[0] == board[4] && board[4] == board[8]) {
+        if (checkHitsInRows()) {
             endGame()
             return true
         }
 
-        if(board[2] != Turn.UNKNOWN && board[2] == board[4] && board[4] == board[6]) {
+        if (checkHitsInColumns()) {
             endGame()
+            return true
+        }
+
+        if (checkHitsInFirstDiagonal()) {
+            endGame()
+            return true
+        }
+
+
+        if(checkHitsInSecondDiagonal()) {
+            endGame()
+            return true
+        }
+
+        // wszystkie pola zajęte
+        if(board.none { it == Turn.UNKNOWN }) {
+            endGameDraw()
             return true
         }
 
         return false
     }
 
+    private fun checkHitsInColumns(): Boolean {
+        for (i in 0..ColumnCount - 1) {
+            val player = board[i]
+            if (player == Turn.UNKNOWN)
+                continue
+
+            for (column in 0..ColumnCount - 1) {
+                var hitsCount = 1
+                for (row in 1..RowsCount - 1) {
+                    if (player == board[ColumnCount * row + column]) {
+                        ++hitsCount
+                    } else {
+                        break
+                    }
+                }
+
+                if (hitsCount == RowsCount) {
+                    return true
+                }
+            }
+
+        }
+        return false
+    }
+
+    private fun checkHitsInRows(): Boolean {
+        for (i in board.indices step ColumnCount) {
+            val player = board[i]
+            if (player == Turn.UNKNOWN)
+                continue
+
+            var samePlayerHits = 1
+            for (cellShift in 1..ColumnCount - 1) {
+                if (player == board[i + cellShift]) {
+                    ++samePlayerHits
+                } else {
+                    break
+                }
+            }
+
+            if (samePlayerHits == ColumnCount) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun checkHitsInFirstDiagonal(): Boolean {
+        val smallerDimension = Math.min(ColumnCount, RowsCount)
+        val player = board[0]
+        if(smallerDimension == 0 || player == Turn.UNKNOWN)
+            return false
+        var hitsCount = 1
+        for (shift in 1..smallerDimension-1) {
+            // sprawdzanie dla przecięć
+            val index = shift * smallerDimension + shift
+            if(player == board[index]) {
+                ++hitsCount
+            } else {
+                break
+            }
+        }
+
+        return hitsCount == smallerDimension
+    }
+
+    private fun checkHitsInSecondDiagonal(): Boolean {
+        val smallerDimension = Math.min(ColumnCount, RowsCount)
+        if(smallerDimension < 1)
+            return false
+
+        val player = board[ColumnCount-1]
+        if(player == Turn.UNKNOWN)
+            return false
+
+        var hitsCount = 1;
+        for(shift in 1..smallerDimension-1) {
+            val index = shift * smallerDimension + ColumnCount - shift - 1
+            if(player == board[index])
+                ++hitsCount
+            else
+                break
+        }
+
+        return hitsCount == smallerDimension
+    }
+
     // TODO: zrobić coś z tym
-    private fun nextPCMove(btn: Button) {
+    private fun nextPCMove() {
         if(!gameInProgress) return
 
         var index = randomMoveGenerator.nextInt(board.size)
         if(board[index] == Turn.UNKNOWN) {
-            board[index] = currentTurn
+            board[index] = pcTurn
+            boardButtons[index]?.text = pcTurn.toString()
         }
+        else {
+            index = board.indexOfFirst { it == Turn.UNKNOWN }
+            board[index] = pcTurn
+            boardButtons[index]?.text = pcTurn.toString()
+        }
+
+        currentTurn = currentTurn.flip()
+        updateStatus()
     }
 
     private fun endGame() {
         textView?.text = String.format("Koniec gry. Wygrał: %s.", currentTurn.verbal)
+        gameInProgress = false
+    }
+
+    private fun endGameDraw() {
+        textView?.text = "Koniec gry. Nikt nie wygrał."
         gameInProgress = false
     }
 
